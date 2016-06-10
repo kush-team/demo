c53942f97f6ae82e1b5fcff1d15615c0eb0176b6
santuan <smrcouto88@gmail.com>
2016-06-10 18:54:54 -0300

    :D

---
+++ backend/api/controllers/PartyController.js
@@ -0,0 +1,11 @@
+/**
+ * BrickController
+ *
+ * @description :: Server-side logic for managing bricks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
